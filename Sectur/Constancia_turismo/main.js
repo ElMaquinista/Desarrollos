ecf5f1e5460c7d1b206520d3c1bfca9ac807e564
Ricardo Garcia <100309156+ElMaquinista@users.noreply.github.com>
2023-10-03 09:28:20 -0600

    nuevo diseño para certificado de sectur

--- Sectur/Constancia_turismo/main.js
+++ Sectur/Constancia_turismo/main.js
@@ -2,9 +2,9 @@
 
 var iframe = null;
 
-function rellenar_llaves_respuestas(){
+function rellenar_llaves_respuestas() {
     let json = {
-        "fecha": "27 de Julio del 2023", 
+        "fecha": "27 de Julio del 2023",
         "clave_control": "1235",
         "nombre": "Ricardo Garcia de la Cruz Sncahez Contador de Atos",
         "calle": "Calle General lucio Blanco",
@@ -14,7 +14,12 @@ function rellenar_llaves_respuestas(){
         "municipio": "Tequisquiapan",
         "no_tramite": "12356778",
         "no_constancia": "040102030404",
-        "servicio_de": "Agencia de administaración de espacio para vehiculos autopropulsados por motor"
+        "servicio_de": "Agencia de administaración de espacio para vehiculos autopropulsados por motor",
+        "letras_pequeñas": "Con fundamento en lo dispuesto en los artículos 42 fracción IV de la Ley Orgánica de la Administración Pública Federal; \n 52 de la Ley General de Turismo y 92 del Reglamento de la Ley General de Turismo. \n Av. Presidente Masaryk No. 172, Piso 2, Col. Bosques de Chapultepec, Alcaldía Miguel Hidalgo, Ciudad de México, C.P.11580 ",
+        "url_logo_sectur": "img/sectur_logo.jpg",
+        "url_sectur": "www.gob.mx/sectur",
+        "nombre_despachador": "Mario Alberto Magdaleno Peralta",
+        "puesto_despachador": "Director de Registro Nacional de Turismo"
     };
 
     // itinerar las llaves para asignar el text en el html
@@ -25,11 +30,27 @@ function rellenar_llaves_respuestas(){
     let nodo_pagina = iframe.document.querySelector('.documento')
     console.log("nodo_pagina", nodo_pagina);
 
-    for(let llave in json){
+    for (let llave in json) {
         let valor = json[llave];
 
+        let nodo = nodo_pagina.querySelector(`[data-json='${llave}']`);
         // buscar la llave en el html
-        
+        if (nodo) {
+            switch (llave) {
+                case "url_logo_sectur":
+                    nodo.src = valor;
+                    break
+                case "letras_pequeñas":
+                    valor = valor.replaceAll('\n', '<br>');
+                    nodo.innerHTML = valor;
+                    break;
+                default:
+                    nodo.innerHTML = valor;
+                    break;
+            }
+        } else {
+            console.warn("Nodo no encontrado: ", llave);
+        }
     }
 
 }

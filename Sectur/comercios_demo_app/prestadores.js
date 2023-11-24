840c62af48aa0de43434f6037bf29f2e87c127b2
Ricardo Garcia <100309156+ElMaquinista@users.noreply.github.com>
2023-11-24 17:35:35 -0600

    json prestadores de servicios ejemplo

---
+++ Sectur/comercios_demo_app/prestadores.js
@@ -0,0 +1,24 @@
+[
+    {
+      "commertial_name": "",
+      "address": "",
+      "id_pst": "",
+      "pst": "",
+      "id_category_pst": "",
+      "category_pst": "",
+      "images": [
+        {
+          "url": ""
+        }
+      ],
+      "telefono": "",
+      "whastapp": "",
+      "email": "",
+      "web": "",
+      "facebook": "",
+      "tweeter": "",
+      "certificate": ""
+    }
+  ]
+  
+  
